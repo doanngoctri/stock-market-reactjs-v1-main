c679d781b229583d57a5cd420199d749a06ffcda
Đoàn Ngọc Trí <tridoanngoc@gmail.com>
2021-10-27 21:50:05 +0700

    resolve

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,9 +1,4 @@
-//const origin = "http://trade-demo.somee.com";
-<<<<<<< HEAD
-//const origin = "http://giaodichchungkhoan-beta-2.somee.com";
 const origin = "https://localhost:5001";
-=======
 const origin = "http://giaodichchungkhoan-beta-2.somee.com";
->>>>>>> 1f8c520ef72c7b260843b274ccbabcd99bc6c770
 export const API_URL = origin + "/api";
 export const BASE_URL = origin;
